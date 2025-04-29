015bb70d20cb5071f712adc3928262c5dbdbe712
Warai777 <rodneysanchez1234@gmail.com>
2025-04-29 00:24:05 -0400

    Update companion_manager.py

--- companion_manager.py
+++ companion_manager.py
@@ -1,21 +1,14 @@
-import json
-import os
 import random
-
-COMPANION_FILE = "data/companions.json"
+from procedural_companion_generator import ProceduralCompanionGenerator
 
 class CompanionManager:
     def __init__(self):
-        if os.path.exists(COMPANION_FILE):
-            with open(COMPANION_FILE, "r") as f:
-                self.all_companions = json.load(f)
-        else:
-            self.all_companions = []
+        pass  # No need to pre-load fixed companions anymore!
 
     def random_companion_encounter(self):
         """Randomly decide if a companion appears. 20% chance."""
         chance = random.randint(1, 100)
-        if chance <= 20 and self.all_companions:
-            return random.choice(self.all_companions)
+        if chance <= 20:
+            return ProceduralCompanionGenerator.generate_companion()
         else:
             return None
