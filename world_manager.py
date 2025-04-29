eb7862f43ea082d4a6a1a0d289974c86e48af4f5
Warai777 <rodneysanchez1234@gmail.com>
2025-04-28 22:14:49 -0400

    Create world_manager.py

---
+++ world_manager.py
@@ -0,0 +1,38 @@
+import random
+import datetime
+import os
+import json
+
+PLAYER_FOLDER = "data/players"
+
+class WorldManager:
+    def __init__(self):
+        self.world_names_pool = [
+            "Sea of Endless Horizons",
+            "Realm of Silent Shadows",
+            "City of Hollow Dreams",
+            "Forest of Distant Echoes",
+            "Vault of Shattered Stars",
+            "Labyrinth of Forgotten Sins",
+            "The Bleeding Mirror",
+            "Twilight Cradle",
+            "The Wounded Sky",
+            "Crown of Falling Leaves"
+        ]
+
+    def generate_books(self):
+        return random.sample(self.world_names_pool, 3)
+
+    def start_world_timer(self, player_name, world_name):
+        filepath = os.path.join(PLAYER_FOLDER, f"{player_name}.json")
+        if not os.path.exists(filepath):
+            return
+
+        with open(filepath, "r") as f:
+            data = json.load(f)
+
+        data["current_world"] = world_name
+        data["world_entry_time"] = datetime.datetime.utcnow().isoformat()
+
+        with open(filepath, "w") as f:
+            json.dump(data, f, indent=2)
